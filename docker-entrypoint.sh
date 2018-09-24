3a7768ce408923561ca28af5aa97061b3ce401da
Abdullah Almariah <abdullahalmariah@gmail.com>
2018-09-24 02:15:20 +0200

    modify docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,16 @@
 set -eo pipefail
 
 if [ ! -z "$KEY_PATH" ]; then
+
+	if [ -z "$GPG_KEY_NAME" ] || [ -z "$GPG_KEY_PASS" ]; then
+		echo "Error: missing GPG_KEY_NAME or GPG_KEY_PASS!" 1>&2
+		exit 1
+	else
+		sed -i -e "s/{{ GPG_KEY_NAME }}/$GPG_KEY_NAME/g" /root/.rpmmacros
+		sed -i -e "s/{{ GPG_KEY_PASS }}/$GPG_KEY_PASS/g" /root/.rpmmacros
+	fi
 	# echo $KEY_PASSPHRASE |
 	gpg --batch --import ${KEY_PATH}
-fi
-
-if [ ! -z "$GPG_KEY_NAME" ]; then
-	sed -i -e "s/{{ GPG_KEY_NAME }}/$GPG_KEY_NAME/g" /root/.rpmmacros
-	sed -i -e "s/{{ GPG_KEY_PASS }}/$GPG_KEY_PASS/g" /root/.rpmmacros
 fi
 
 exec "$@"
